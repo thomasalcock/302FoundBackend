63529bf97cd3a3585a56341c6ee908bd6ab8f29a
villmox-laptop <pderdorfbewohner@gmail.com>
2025-10-25 12:34:30 +0200

    basic axum server

--- threeotwo-backend/src/main.rs
+++ threeotwo-backend/src/main.rs
@@ -1,11 +1,21 @@
+use axum::Router;
+use tokio::net::TcpListener;
+
 
 #[tokio::main]
 async fn main() {
     println!("STARTING WEB SERVER");
-    //
+
+    //merge routes here
+    let app = Router::new();
     
 
-    //run
 
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    println!("SERVER IS SHUT DOWN");
 }
