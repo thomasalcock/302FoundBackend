d7f6edb83a05508302890ad6b2c6af57aab1c804
villmox-laptop <pderdorfbewohner@gmail.com>
2025-10-25 13:13:32 +0200

    added user routes

---
+++ threeotwo-backend/src/user/mod.rs
@@ -0,0 +1,25 @@
+use axum::{routing::{get, post}, Router};
+
+
+pub fn user_routes() -> Router {
+    Router::new()
+        .route("/create", post(create_user))
+        .route("/update", post(update_user))
+        .route("/read", get(read_user))
+        .route("/delete", post(delete_user))
+}
+
+async fn create_user() {
+    todo!();
+}
+
+async fn update_user() {
+    todo!();
+}
+
+async fn read_user() {
+    todo!();
+}
+
+async fn delete_user() {}
+
