6a950a6ba45141dcb8b34edc758660c49ed0ad67
Moritz Schüßler <mail@moritzschuessler.de>
2025-10-25 20:20:09 +0200

    feat: add cors rules

--- threeotwo-backend/src/main.rs
+++ threeotwo-backend/src/main.rs
@@ -2,9 +2,10 @@ use std::env;
 
 use axum::response::Response;
 use axum::routing::any;
-use axum::{middleware, Router};
+use axum::{Router, middleware};
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
+use tower_http::cors::CorsLayer;
 
 mod error;
 use crate::error::Error;
@@ -15,20 +16,19 @@ mod user;
 pub struct AppState {}
 impl AppState {
     fn new(_database: &str) -> Self {
-       AppState {} 
+        AppState {}
     }
 }
-
 
 #[tokio::main]
 async fn main() {
     println!("STARTING WEB SERVER");
-    
+
     let app_state = AppState::new("db.sqlite");
 
     let port = match env::var("BACKEND_PORT") {
         Ok(p) => p,
-        Err(_) => "3000".to_string()
+        Err(_) => "3000".to_string(),
     };
 
     //merge routes here
@@ -38,11 +38,12 @@ async fn main() {
         .nest("/auth", auth::routes())
         .layer(middleware::map_response(map_all_responses))
         .layer(CookieManagerLayer::new())
+        .layer(CorsLayer::permissive())
         .route("/{*wildcard}", any(static_error));
 
     let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
-.unwrap();
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 
     println!("SERVER IS SHUT DOWN");
